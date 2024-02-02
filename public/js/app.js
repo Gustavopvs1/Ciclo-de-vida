d54123c08b3d44a736968644ceda3cb89a0d107e
raul <2021171023@uteq.edu.mx>
2024-02-02 17:12:27 -0600

    ciclo de vida

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+    // .then(reg => {
+    //     console.log('Service Worker registered', reg)
+    // }).catch(err => {
+    //     console.error('Service Worker not registered', err)
+    // });
 }
+fetch('https://fakestoreapi.com/products/1')
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,22 @@
-// Responder con lo que contiene la solicitud
+// ciclo de vida del sw
+//1.- install
+self.addEventListener('install', event => {
+    console.log('Service Worker installed 1', event);
+    const instalation = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Finished pre-caching static assets.');
+            self.skipWaiting();
+            resolve();
+        }, 1);
+    });
+    event.waitUntil(instalation);
+});
+//2.- activate
+self.addEventListener('activate', event => {
+    console.log('Service Worker activated 2', event);
+});
+//3.- fetch
 self.addEventListener('fetch', event => {
-    console.log(event);
+    console.log('Service Worker fetching 3', event.request.url);
+    // event.respondWith(fetch(event.request));
 });
